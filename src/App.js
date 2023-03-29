d5211ddeb95549640c710260f22271f4b300c52a
kunalkaklotar <kunalkaklotar@gmail.com>
2023-03-29 18:08:24 +0530

    project2

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import logo from './logo.svg';
+// import './App.css';
+import Header from './Header';
+import Footer from './Footer.js'
+import Main from './Main.js'
+
+
+function App() {
+  return (
+    <div>
+      <header>
+        <Header/>
+        <Main/>
+        <Footer/>
+      </header>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Header.js
@@ -0,0 +1,28 @@
+import React from 'react'
+import './header.css';
+
+
+function Header() {
+  return (
+    <div>
+            <div className="d_flex">
+                <div className='ok'>
+                </div>
+                <ul className="d_flex">
+                    <li>Home
+                        <ol>
+                            <li></li>
+                        </ol>
+                    </li>
+                    <li>About Us</li>
+                    <li>Language</li>
+                    <li>Blog</li>
+                    <li>Featured</li>
+                    <li>Contect Us</li>
+                </ul>
+            </div>
+        </div>
+  )
+}
+
+export default Header
