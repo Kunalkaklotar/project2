9058b026a10cb6e41888fb18a30e3f0a3b0742b6
kunalkaklotar <kunalkaklotar@gmail.com>
2023-03-29 18:54:19 +0530

    project2

--- src/Header.js
+++ src/Header.js
@@ -4,7 +4,7 @@ import './header.css';
 
 function Header() {
   return (
-    <div>
+    <div className="">
             <div className="d_flex">
                 <div className='ok'>
                 </div>
